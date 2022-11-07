c2a471dbd2e604fc9e7e3b2640771166de7c231b
camailo19 <camiloguerra@uninorte.edu.co>
2022-11-07 01:30:11 -0500

    new commit

--- Assets/Scripts/ClickController.cs
+++ Assets/Scripts/ClickController.cs
@@ -12,13 +12,15 @@ public class ClickController : MonoBehaviour
     GameObject maintower;
     GameObject newtower;
     GameObject playerX;
+    GameObject lasttower;
     string[] matlist;
     string[] matlist2;
     string[] matlist3;
-    string myFilePath, fileName; string myFilePath2, fileName2; string myFilePath3, fileName3;
+    string myFilePath, fileName; string myFilePath2, fileName2; string myFilePath3, fileName3; string myFilePath4, fileName4;
     string txtDocumentName;
     string txtDocumentName2;
     string txtDocumentName3;
+    string txtDocumentName4;
     public bool isplaying=false;
 
 
@@ -27,17 +29,30 @@ public class ClickController : MonoBehaviour
 
     private TowerBehaviour myTower;
 
-
-
     List<Vector3> paths = new List<Vector3>();
     List<Vector3> positionxy = new List<Vector3>();
 
     List<GameObject> towers = new List<GameObject>();
+    List<GameObject> towersloads = new List<GameObject>();
     List<GameObject> players = new List<GameObject>();
 
 
-    List<GameObject> lasttowerposition = new List<GameObject>();
+    int qtoweri;
+    int qtowerii;
+    int qtoweriii;
+    int qplayeri;
+    int qplayerii;
+    int qplayeriii;
+    int qplayeriv;
 
+
+
+    List<Vector3> lasttowerposition = new List<Vector3>();
+
+    List<int>  towertype = new List<int>();
+
+    float timeofplaying=0.0f;
+    float nexttime=0.0f;
 
 
 
@@ -92,30 +107,51 @@ public class ClickController : MonoBehaviour
        
         if (isplaying == true)
         {
+
             Debug.Log("Estoy revisando");
             Debug.Log("Valor Restante Torres:"+ Enemies.enemies.Count);
             Debug.Log("Valor Restante Jugadores:"+Allies.allies.Count);
 
-
-
-            if (Enemies.enemies.Count == 0 )
+            if (Enemies.enemies.Count == 1 )
             {
+                nexttime = Time.time;
+
+                File.AppendAllText(txtDocumentName4, "\n ----------Resultado--------------");
+                File.AppendAllText(txtDocumentName4, "\n");
+                File.AppendAllText(txtDocumentName4, "\n Ganaron los gatos con "+Allies.allies.Count+"unidades restantes y un tiempo de "+(nexttime-timeofplaying));
+                File.AppendAllText(txtDocumentName4, "\n");
+
                 matlist3 = new string[20];
                 Debug.Log("Ganaron los players");
-                matlist3[0] = "Los gatos son los ganadores con: "+Allies.allies.Count+" unidades restantes";
-                File.WriteAllLines(txtDocumentName3, matlist3);
+                //matlist3[0] = "Nueva Partida";
+                //matlist3[1] = "Los gatos son los ganadores con: "+Allies.allies.Count+" unidades restantes";
+                File.AppendAllText(txtDocumentName4, "\n");
+                File.AppendAllText(txtDocumentName3, "\n ----------Nueva Partida--------------");
+                File.AppendAllText(txtDocumentName4, "\n");
+                File.AppendAllText(txtDocumentName3, "\n ganaron los gatos, slay");
+                File.AppendAllText(txtDocumentName4, "\n");
                 isplaying = false;
+                Load();
             }
 
 
             if (Allies.allies.Count == 0)
             {
+                nexttime = Time.time;
                 matlist3 = new string[20];
                 Debug.Log("Ganaron los towers");
-                matlist3[0] = "Las torres son los ganadores con: " + Enemies.enemies.Count + " unidades restantes";
-                File.WriteAllLines(txtDocumentName3, matlist3);
-                isplaying = false;
+                //  matlist3[0] = "Nueva Partida";
+                //  matlist3[1] = "Las torres son los ganadores con: " + Enemies.enemies.Count + " unidades restantes";
+                File.AppendAllText(txtDocumentName4, "\n");
+                File.AppendAllText(txtDocumentName4, "\n ----------Resultado--------------");
+                File.AppendAllText(txtDocumentName4, "\n");
+                File.AppendAllText(txtDocumentName4, "\n Ganaron las torres con " + Enemies.enemies.Count + "unidades restantes y un tiempo de " + (nexttime - timeofplaying));
+                File.AppendAllText(txtDocumentName4, "\n");
+                File.AppendAllText(txtDocumentName3, "\n ----------Nueva Partida--------------");
+                File.AppendAllText(txtDocumentName3, "\n ganaron las torres");
 
+                isplaying = false;
+                Load();
             }
             
         }
@@ -127,16 +163,17 @@ public class ClickController : MonoBehaviour
         txtDocumentName = Application.streamingAssetsPath + "/Chat_Logs/" + "Jugadores" + ".txt";
         txtDocumentName2 = Application.streamingAssetsPath + "/Chat_Logs/" + "Torres" + ".txt";
         txtDocumentName3 = Application.streamingAssetsPath + "/Chat_Logs/" + "GanadoryPerdedor" + ".txt";
-
+        txtDocumentName4 = Application.streamingAssetsPath + "/Chat_Logs/" + "Historial de Batalla" + ".txt";
     }
 
 
     public void delete()
     {
-        Destroy(maintower);
+ 
 
         foreach (var objects in towers)
         {
+            Enemies.enemies.Remove(objects);
             Destroy(objects);
         }
 
@@ -149,6 +186,7 @@ public class ClickController : MonoBehaviour
 
         foreach (var objects in players)
         {
+            Allies.allies.Remove(objects);
             Destroy(objects);
         }
 
@@ -175,12 +213,20 @@ public class ClickController : MonoBehaviour
 
     public void start()
     {
-        deletePlayers();
-        delete();
         RandomVOIPosition2();
         RandomPlacePlayer();
+        timeofplaying = Time.time;
         isplaying = true;
     }
+
+    public void CSV()
+    {
+
+
+
+    }
+
+
 
 
 
@@ -191,17 +237,53 @@ public class ClickController : MonoBehaviour
     }
 
 
-    public void calllastTowerPosition()
+    public void Load()
     {
-        foreach (var objectss in lasttowerposition)
+        deletePlayers();
+        delete();
+        for (int i = 0; i < lasttowerposition.Count; i++)
         {
-            Debug.Log(objectss);
+
+            if (towertype[i] == 0)
+            {
+                lasttower = (GameObject)Instantiate(GameManager.Instance.TowerPrefab, lasttowerposition[i], Quaternion.identity);
+                towersloads.Add(lasttower);
+                towers.Add(lasttower);
+        
+            }
+
+
+            if (towertype[i] == 2)
+            {
+                lasttower = (GameObject)Instantiate(GameManager.Instance.TowerPrefab2, lasttowerposition[i], Quaternion.identity);
+                towersloads.Add(lasttower);
+                towers.Add(lasttower);
+            }
+
+
+            if (towertype[i] == 3)
+            {
+                lasttower = (GameObject)Instantiate(GameManager.Instance.TowerPrefab3, lasttowerposition[i], Quaternion.identity);
+                towersloads.Add(lasttower);
+                towers.Add(lasttower);
+            }
+
+
+            if (towertype[i] == 4)
+            {
+                lasttower = (GameObject)Instantiate(GameManager.Instance.TowerPrefab4, lasttowerposition[i], Quaternion.identity);
+                towersloads.Add(lasttower);
+                towers.Add(lasttower);
+            }
         }
+        RandomPlacePlayer();
+        timeofplaying = Time.time;
+        isplaying = true;
     }
 
     public void deletelastTowerPosition()
     {
-        foreach (var objects in lasttowerposition)
+        foreach (var objects in towersloads)
         {
             Destroy(objects);
         }
@@ -211,7 +293,11 @@ public class ClickController : MonoBehaviour
 
     public void RandomPlacePlayer()
     {
-
+        File.AppendAllText(txtDocumentName4, "\n");
+        File.AppendAllText(txtDocumentName4, "\n ----------Nueva Partida--------------");
+        File.AppendAllText(txtDocumentName4, "\n");
+        File.AppendAllText(txtDocumentName4, "\n ----------Formación Jugadores--------------");
+        File.AppendAllText(txtDocumentName4, "\n");
         int contjugadores = 0;
         int maxcostplayer = 36;
         int randomPositionPlayer;
@@ -245,6 +331,10 @@ public class ClickController : MonoBehaviour
                     matlist[contador] = " Posición " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador;
                     contador++;
                     contjugadores++;
+
+                    File.AppendAllText(txtDocumentName4, "\n "+" Posición: "+ positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador);
+
+
                 }
 
                 if (randomPlayerType == 2 && maxcostplayer >= 5)
@@ -260,6 +350,8 @@ public class ClickController : MonoBehaviour
                     matlist[contador] = " Posición " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador;
                     contador++;
                     contjugadores++;
+                    File.AppendAllText(txtDocumentName4, "\n " + " Posición: " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador);
+
                 }
 
                 if (randomPlayerType == 3 && maxcostplayer >= 2)
@@ -275,6 +367,8 @@ public class ClickController : MonoBehaviour
                     matlist[contador] = " Posición " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador;
                     contador++;
                     contjugadores++;
+                    File.AppendAllText(txtDocumentName4, "\n " + " Posición: " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador);
+
                 }
 
                 if (randomPlayerType == 4 && maxcostplayer >= 5)
@@ -290,6 +384,7 @@ public class ClickController : MonoBehaviour
                     matlist[contador] = " Posición " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador;
                     contador++;
                     contjugadores++;
+                    File.AppendAllText(txtDocumentName4, "\n " + " Posición: " + positionxy[randomPositionPlayer] + " Tipo de Jugador: " + tipodejugador);
                 }
 
 
@@ -302,12 +397,16 @@ public class ClickController : MonoBehaviour
         }
              matlist[contador] = "Numero de Jugadores: " + contjugadores; 
             File.WriteAllLines(txtDocumentName, matlist);
+        File.AppendAllText(txtDocumentName4, "\n " + "Numero de Jugadores: " + contjugadores);
 
     }
 
 
     public void RandomVOIPosition2()
     {
+        File.AppendAllText(txtDocumentName4, "\n");
+        File.AppendAllText(txtDocumentName4, "\n ----------Formación Torres--------------");
+        File.AppendAllText(txtDocumentName4, "\n");
         int contprecio = 50;
         int randomNum;
         int conttorres = 0;
@@ -319,8 +418,12 @@ public class ClickController : MonoBehaviour
         int contador = 0;
         matlist2 = new string[quantity];
         string tipodetorre = "";
+       towertype.Add(0);
+       lasttowerposition.Add(new Vector3(-17.9f, 0.9f, 0.0f));
 
-        lasttowerposition.Add(maintower);
+        towers.Add(maintower);
+
+        File.AppendAllText(txtDocumentName4, "\n"+"Posición  (-17.9,0.9,0.0)"+"Tipo de Torre: Principal");
 
 
         while (contprecio >= 5 && conttorres < 8)
@@ -339,10 +442,12 @@ public class ClickController : MonoBehaviour
                     numeros.Add(randomNum);
                     towers.Add(newtower);
                     conttorres++;
-
-                    lasttowerposition.Add(newtower);
+                    towertype.Add(2);
+                    lasttowerposition.Add(paths[randomNum]);
                     tipodetorre = "Torre 1";
                     matlist2[contador] = " Posición " + paths[randomNum] + " Tipo de Torre: " + tipodetorre;
+                    File.AppendAllText(txtDocumentName4, "\n Posición " + paths[randomNum] + "Tipo de Torre:" + tipodetorre);
+
                     contador++;
                 }
 
@@ -355,10 +460,11 @@ public class ClickController : MonoBehaviour
                     towers.Add(newtower);
                     conttorres++;
 
-
-                    lasttowerposition.Add(newtower);
+                    towertype.Add(3);
+                    lasttowerposition.Add(paths[randomNum]);
                     tipodetorre = "Torre 2";
                     matlist2[contador] = " Posición " + paths[randomNum] + " Tipo de Torre: " + tipodetorre;
+                    File.AppendAllText(txtDocumentName4, "\n Posición " + paths[randomNum] + "Tipo de Torre:" + tipodetorre);
                     contador++;
 
                 }
@@ -373,10 +479,12 @@ public class ClickController : MonoBehaviour
                     towers.Add(newtower);
                     conttorres++;
 
-                    lasttowerposition.Add(newtower);
 
+                    lasttowerposition.Add(paths[randomNum]);
+                    towertype.Add(4);
                     tipodetorre = "Torre 3";
                     matlist2[contador] = " Posición " + paths[randomNum] + " Tipo de Torre: " + tipodetorre;
+                    File.AppendAllText(txtDocumentName4, "\n Posición " + paths[randomNum] + "Tipo de Torre:" + tipodetorre);
                     contador++;
 
 
@@ -389,7 +497,7 @@ public class ClickController : MonoBehaviour
 
         matlist2[contador] = "\n Numero de Torres: " + conttorres;
         File.WriteAllLines(txtDocumentName2, matlist2);
-
+        File.AppendAllText(txtDocumentName4, "\n Número de Torres: "+ conttorres);
 
 
     }
